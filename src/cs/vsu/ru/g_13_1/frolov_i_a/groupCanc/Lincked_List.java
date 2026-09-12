c373844ffd741c4788f32e26549d52e5e99e41e7
Frolov Ilia <sashakroos852369@gmail.com>
2026-09-12 14:56:31 +0300

    Просто реализация связоного списка и его методы

---
+++ src/cs/vsu/ru/g_13_1/frolov_i_a/groupCanc/Lincked_List.java
@@ -0,0 +1,102 @@
+package cs.vsu.ru.g_13_1.frolov_i_a.groupCanc;
+
+
+public class MyLinkList {
+    Node Head;
+    Node Tail;
+    public void addFirst(int value){
+        Node newNode =  new Node(value);
+        if(Head == null){
+            Head = newNode;
+            Tail = newNode;
+        }else {
+            newNode.next = Head;
+            Head.prev = newNode;
+            Head = newNode;
+        }
+    }
+    public void addLast(int value){
+        Node newNode =  new Node(value);
+        if(Head == null){
+            Head = newNode;
+            Tail = newNode;
+        }else {
+            newNode.prev = Tail;
+            Tail.next = newNode;
+            Tail = newNode;
+        }
+    }
+    public void removeFirst() {
+        if (Head == null) {
+            return;
+        } else if (Head == Tail) {
+            Head = null;
+            Tail = null;
+        } else {
+            Head = Head.next;
+            Head.prev = null;
+        }
+    }
+    public void removeLast(){
+        if (Head == null) {
+            return;
+        } else if (Head == Tail) {
+            Head = null;
+            Tail = null;
+        }else{
+            Tail = Tail.prev;
+            Tail.next = null;
+        }
+
+    }
+    //задача от ии на поиск элемента
+    public boolean contains(int value){
+        Node curr = Head;
+        for (curr = Head; curr != null ; curr = curr.next) {
+            if(curr.value == value){
+                return true;
+            }
+        }
+        return false;
+    }
+    //задача от ии на вывод списка с конца
+    public void printReverse(){
+        Node curr = Tail;
+        while (curr != null){
+            System.out.print(curr.value + " -> ");
+            curr = curr.prev;
+        }
+        System.out.println("null");
+
+    }
+    //задача от ии на вырез числа
+    public boolean removeByValue(int value) {
+        Node curr = Head;
+
+        for (curr = Head; curr != null; curr = curr.next) {
+            if (curr.value == value) {
+                if (curr == Head) {
+                    removeFirst();
+                } else if (curr == Tail) {
+                    removeLast();
+                } else {
+                    curr.prev.next = curr.next;
+                    curr.next.prev = curr.prev;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    public void printList(){
+        Node curr = Head;
+        while (curr != null){
+            System.out.print(curr.value + " -> ");
+            curr = curr.next;
+        }
+        System.out.println("null");
+
+    }
+
+}
---
+++ src/cs/vsu/ru/g_13_1/frolov_i_a/groupCanc/Node.java
@@ -0,0 +1,13 @@
+package cs.vsu.ru.g_13_1.frolov_i_a.groupCanc;
+
+
+public class Node {
+    int value;
+    Node next;
+    Node prev;
+
+    public Node(int value){
+        this.value = value;
+    }
+}
+
